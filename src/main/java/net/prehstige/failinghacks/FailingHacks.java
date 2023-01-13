81f1939bba3ccb98a4f6b5d685a1b8736edb8e95
Prehstige <liebert.artjom@gmail.com>
2023-01-13 15:53:03 +0100

    Initial commit

---
+++ src/main/java/net/prehstige/failinghacks/FailingHacks.java
@@ -0,0 +1,15 @@
+package net.prehstige.failinghacks;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FailingHacks implements ModInitializer {
+	public static final String MOD_ID = "failinghacks";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/prehstige/failinghacks/FailingHacksClient.java
@@ -0,0 +1,11 @@
+package net.prehstige.failinghacks;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FailingHacksClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
